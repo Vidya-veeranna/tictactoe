35fc561479e8240395340632c2859615ad36828d
vidya-coder <vidyagowda191@gmail.com>
2021-06-13 08:50:00 +0530

    update

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Board from './components/Board';
+
+const App = () => {
+  return (
+    <div className="app">
+      <h2>TIC TAC TOE</h2>
+      <Board />
+    </div>
+  );
+};
+
+export default App;
